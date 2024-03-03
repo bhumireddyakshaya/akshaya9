6700f264faa8a2da1ef8dd4129abbe0447fde016
bhumireddyakshaya <161736877+bhumireddyakshaya@users.noreply.github.com>
2024-03-03 23:43:20 +0530

    Add files via upload

---
+++ string palindrome.py
@@ -0,0 +1,5 @@
+def is_palindrome(s):
+    s = ''.join(char.lower() for char in s if char.isalnum())
+    return s == s[::-1]
+print(is_palindrome("A man, a plan, a canal: Panama"))  # True
+print(is_palindrome("race a car"))  # False
